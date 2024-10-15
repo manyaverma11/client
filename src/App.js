0532c6fc107a61ebfde411d7d44712417bbaae2b
Manya <manyaverma0154@gmail.com>
2024-10-15 16:33:57 +0530

    Add SocketProvider and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Home";
+import { SocketProvider } from "./providers/Socket";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/room/:roomId" element={<h1>Hey you are in room</h1>} />
+        </Routes>
+      </SocketProvider>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React, {useState,useEffect} from "react";
+import {useSocket} from "../providers/Socket"
+import {useNavigate} from "react-router-dom"
+
+const Homepage = () => {
+  const {socket} = useSocket();
+  const navigate = useNavigate();
+  
+  const [email, setEmail] = useState();
+  const [roomId, setRoomId] = useState();
+
+  const handleRoomJoined = ({roomId}) => {
+    navigate(`/room/${roomId}`)
+  }
+
+  useEffect(()=>{
+    socket.on("joined-room", handleRoomJoined)
+  },[socket])
+
+  const handleJoinRoom = () => {
+    socket.emit('join-room', {emailId : email, roomId})
+  }
+
+  return (
+    <div className="homepage-container">
+      <div className="input-container">
+        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Enter your email" />
+        <input value={roomId} onChange={(e)=>setRoomId(e.target.value)} type="text" placeholder="Enter room code" />
+        <button onClick={handleJoinRoom}>Enter Room</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
